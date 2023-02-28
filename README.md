7d7b7927ce5d382022ed9c093b2f848d7e3aacd4
Payal (Alisha) Dhillon <116124181+Payal-dh@users.noreply.github.com>
2023-02-27 19:27:22 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,43 @@
+# HealthCare Employee Attrition
+
+## An analysis on employee retention and attrition rates by various factors based on the healthcare sector database in the US
+
+Link to Slide: https://docs.google.com/presentation/d/1UWP7KtaOD43Nu9XVmUkzutt46SMLdwcy53d_GTyMzyA/edit?usp=sharing
+
+<img width="403" alt="Screenshot 2023-02-25 at 7 08 48 PM" src="https://user-images.githubusercontent.com/106120403/221385278-544fe220-3804-41d3-970f-4b8f8bdde1d4.png">
+
+Contributors: Jane, Payal Dhillon (Alisha), Andy, Ose
+
+Data source: Kaggle.com 
+
+Project goal: Evaluate employee retention and attrition rates by various factors based on the healthcare sector database in the US.
+
+database is used to house the data: health.sqlite (http://127.0.0.1:5000/api/v1.0/health)
+
+## Research Questions
+
+### Retention rate vs gender
+
+<img width="873" alt="Screenshot 2023-02-25 at 7 29 44 PM" src="https://user-images.githubusercontent.com/106120403/221385869-c1784ec0-0123-465c-8ab8-ac1482289949.png">
+
+### Retention rate vs Marital Status
+
+<img width="979" alt="Screenshot 2023-02-25 at 7 22 19 PM" src="https://user-images.githubusercontent.com/106120403/221385682-d4b6c0f2-9b5e-4b69-a67f-e5e09070cbb4.png">
+
+### Retention rate vs department
+
+<img width="1067" alt="Screenshot 2023-02-25 at 7 33 52 PM" src="https://user-images.githubusercontent.com/106120403/221385974-0d61e4c5-f959-4bc9-9e2c-ca2868fc1538.png">
+
+### Retention rate vs Job Satisfaction
+
+<img width="716" alt="Screenshot 2023-02-25 at 7 35 42 PM" src="https://user-images.githubusercontent.com/106120403/221386032-f79cb88e-9b6b-4cfa-9d15-2391689f55ad.png">
+
+### Employees Hourly Rates vs Attrition
+
+<img width="1141" alt="Screenshot 2023-02-26 at 7 35 09 AM" src="https://user-images.githubusercontent.com/106120403/221410785-637d8d20-4a81-42cc-806a-3745cab5d3d9.png">
+
+### Total Number of Attrition
+![download](https://user-images.githubusercontent.com/116124181/221476592-5aab1c69-b801-4184-b49f-86e8848330ed.png)
+
+Reference: https://www.kaggle.com/datasets/925f54cca84887ec452f1ae1cd430ba9b37cfa555c2e1575760c3f2265c3a696
+
---
+++ file/static/js/app.js
@@ -0,0 +1,185 @@
+const file = "/api/v1.0/health";
+const NOT_SATISFIED_VALUE = 1;
+
+let dropdown_gender = d3.select("#selDataset");
+let dataset_gender = dropdown_gender.property("value");
+
+let dropdown_marital = d3.select("#selDataset2");
+let dataset_marital = dropdown_marital.property("value");
+
+let dropdown_department = d3.select("#selDataset3");
+let dataset_department = dropdown_department.property("value");
+
+let dropdown_job = d3.select("#selDataset4");
+let dataset_job = dropdown_job.property("value");
+
+function init() {
+    buildgenderpieChart("Male");
+    buildbarChart("Married");
+    buildDepartmentBarChart("Cardiology");
+    buildJobSatisfactionpieChart(NOT_SATISFIED_VALUE);
+};
+
+function optionChangedGender(x) {
+    buildgenderpieChart(x);
+};
+
+function optionChangedmarital(x) {
+    buildbarChart(x);
+};
+
+function optionChangedDepartment(x) {
+    buildDepartmentBarChart(x);
+};
+
+function optionChangedJobSatisfaction(x) {
+    buildJobSatisfactionpieChart(x);
+};
+
+function buildgenderpieChart(x){
+    d3.json(file).then((data) => {
+      console.log(data)
+    var resultArray = data.filter(sampleObj => sampleObj.Gender== x);
+    console.log(resultArray)
+    var totalGenderEmployee = resultArray.length;
+    var yesArray = resultArray.filter(sampleObj => sampleObj.Attrition== "Yes");
+    var noArray = resultArray.filter(sampleObj => sampleObj.Attrition== "No");
+    let yes_att = yesArray.length;
+    let no_att = noArray.length;
+    let AttritionRate = yes_att / totalGenderEmployee;
+    console.log(yes_att, no_att);
+
+    const gendertrace = [{
+        values: [AttritionRate, 1 - AttritionRate],
+        labels: ['Attrition', 'No Attrition'],
+        type: 'pie'
+    }];
+
+    const genderlayout = {
+        title: 'Employee Gender vs. Attrition Rates'
+    };
+
+    Plotly.newPlot('gender_pie', gendertrace, genderlayout);
+    });
+};
+
+function buildbarChart(x){
+    d3.json(file).then((data) => {
+      console.log(data)
+      var resultArray = data.filter(sampleObj => sampleObj.MaritalStatus== x);
+      console.log(resultArray)
+      var yesArray = resultArray.filter(sampleObj => sampleObj.Attrition== "Yes");
+      var noArray = resultArray.filter(sampleObj => sampleObj.Attrition== "No");
+      let yes_att = yesArray.length;
+      let no_att = noArray.length;
+      console.log(yes_att, no_att)
+      var bartrace = [{
+        x: ['Yes', 'No'],
+        y: [yes_att, no_att],
+        name: 'Marital Status',
+        type: 'bar',
+        marker: {color:["green", "red"]}
+      }];
+      var barlayout = {title: 'Marital Status vs Attrition'};
+      Plotly.newPlot('marital_bar', bartrace, barlayout);
+    });
+};
+
+function buildDepartmentBarChart(x){
+    d3.json(file).then((data) => {
+      console.log(data)
+      var resultArray = data.filter(sampleObj => sampleObj.Department== x);
+      console.log(resultArray)
+      var yesArray = resultArray.filter(sampleObj => sampleObj.Attrition== "Yes");
+      var noArray = resultArray.filter(sampleObj => sampleObj.Attrition== "No");
+      let yes_att = yesArray.length;
+      let no_att = noArray.length;
+      console.log(yes_att, no_att)
+      var bartrace = [{
+        x: [yes_att, no_att],
+        y: ['Yes', 'No'],
+        name: 'Department',
+        type: 'bar',
+        orientation: 'h',
+        marker: {color:["green", "yellow"]}
+      }];
+      var barlayout = {
+        title: 'Department vs Attrition'};
+      Plotly.newPlot('department_bar', bartrace, barlayout);
+    });
+};
+
+function buildJobSatisfactionpieChart(x){
+    d3.json(file).then((data) => {
+      console.log(data)
+    var resultArray = data.filter(sampleObj => sampleObj.JobSatisfaction== x);
+    console.log(resultArray)
+    var totalJobSatisfaction = resultArray.length;
+    var yesArray = resultArray.filter(sampleObj => sampleObj.Attrition== "Yes");
+    var noArray = resultArray.filter(sampleObj => sampleObj.Attrition== "No");
+    let yes_att = yesArray.length;
+    let no_att = noArray.length;
+    let AttritionRate = yes_att / totalJobSatisfaction;
+    console.log(yes_att, no_att);
+    const jobtrace = [{
+        values: [AttritionRate, 1 - AttritionRate],
+        labels: ["Attrition", "No Attrition"],
+        type: "pie",
+        hole: .4,
+        marker: {
+            colors:["yellow", "blue"]}
+    }];
+    const joblayout = {
+        title: "Job Satisfaction Attrition Rates"
+    };
+    Plotly.newPlot("JobSatisfaction_pie", jobtrace, joblayout);
+    });
+};
+
+init();
+
+var chart1 = Highcharts.chart("container", {
+  chart: {
+    type: 'bar'
+  },
+  title: {
+      text: 'Employee Hourly Rate Groups and Attrition Rates',
+      align: 'left'
+  },
+
+  xAxis: {
+    categories: ['30-39', '40-49', '50-59', '60-69', '70-79', '80-89', '90-100'], 
+        title: {
+          text: 'Hourly Rate Ranges (USD)'
+        }
+    },
+
+  yAxis: {
+      title: {
+          text: 'Number of Retention & Attrition'
+      }
+  },
+
+  plotOptions: {
+    bar: {
+        dataLabels: {
+            enalbed: true
+        }
+    }
+  },
+
+  legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle'
+  },
+
+  series: [{
+      name: 'Number of Retention',
+      data: [172, 235, 207, 193, 213, 230, 233]
+  }, {
+      name: 'Number of Attrition',
+      data: [27, 31, 38, 32, 30, 25, 23]
+  }],
+
+});
